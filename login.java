01fbf06730d2d8f6e50e28a126a8ddb1a23d4aa0
Vinitha930 <96111104+Vinitha930@users.noreply.github.com>
2023-01-07 22:34:08 +0530

    Update login.java

--- login.java
+++ login.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
